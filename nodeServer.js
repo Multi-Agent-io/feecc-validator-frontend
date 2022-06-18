1ddca9266ba47717a3acc83a3bf3abc637fa5cd8
Nikolas <timofeevnik41@gmail.com>
2022-06-18 14:50:52 +0300

    :zap: Codestyle base

--- nodeServer.js
+++ nodeServer.js
@@ -1,21 +1,23 @@
-const path = require('path')
-const express = require('express')
-const app = express()
+const path = require('path');
+const { Console } = require('console');
+const express = require('express');
+
+const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function (req, res){
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
-console.log(`Server started on port: ${port}`)
+Console.log(`Server started on port: ${port}`);
 
-app.listen(port)
+app.listen(port);
 
-const process = require('process')
+const process = require('process');
 
 process.on('SIGINT', () => {
-  console.info("Shutting down NodeJS server")
-  process.exit(0)
-})
+  Console.info('Shutting down NodeJS server');
+  process.exit(0);
+});
